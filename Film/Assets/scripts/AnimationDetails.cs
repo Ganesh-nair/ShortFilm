53635ab8d166d6af348a80d6118343617dc102ca
Ganesh-nair <ganesh.nair.2022@dicevfs.co.in>
2021-05-15 13:14:27 +0530

    Zombie animation v3

---
+++ Film/Assets/scripts/AnimationDetails.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class AnimationDetails
+{
+    public string animationState;
+    public int weight;
+}
---
+++ Film/Assets/scripts/PlayerAnimationController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private List<AnimationDetails> _animationList;
+
+    private int _totalWeight;
+    System.Random _random;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _random = new System.Random();
+        _totalWeight = GetTotalWeight();
+        PlayAnimationState();
+    }
+
+    private void PlayAnimationState() {
+        int randomValue = _random.Next(0, _totalWeight); 
+        Debug.Log("Random:" + randomValue);
+
+        foreach(AnimationDetails animationDetails in _animationList) {
+            randomValue -= animationDetails.weight;
+
+            if(!(randomValue <= 0)) {
+                continue;
+            }
+
+            _animator.Play(animationDetails.animationState);
+            return;
+        }
+    }
+
+    private void OnAnimationComplete() {
+        PlayAnimationState();
+    }
+
+    private int GetTotalWeight() {
+        int weight = 0;
+
+        foreach(AnimationDetails animationDetails in _animationList) {
+            weight += animationDetails.weight;
+        }
+
+        return weight;
+    }
+ }
